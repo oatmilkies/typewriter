aa0e1e57d25bda5f205affa1f49661ed82d2befd
Leilani <pquaby@gmail.com>
2024-02-13 16:54:07 -0500

    added comments

--- typewriter.js
+++ typewriter.js
@@ -4,10 +4,12 @@ const sentence = "hello there from lighthouse labs";
 
 let t = 0;
 
+//Print each letter one by one on a single line
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, t += 100);
 }
 
+//Add new line at the end
 setTimeout(() => console.log(), t);
